dfeda68255b437907d5ace35398c68d6a31e7a46
luanrnunes <luanrobertonunes@hotmail.com>
2021-06-13 02:31:59 -0300

    Conexao ao Banco de dados

--- src/model/services/RelacaoCliente.java
+++ src/model/services/RelacaoCliente.java
@@ -1,19 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class RelacaoCliente {
-
+	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 	public List <Department> findAll()
 	{
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Ciser"));
-		list.add(new Department(2, "Tupy"));
-		list.add(new Department(3, "Hospital"));
-		return list;
+		return dao.findAll();
 	}
 	
 }
